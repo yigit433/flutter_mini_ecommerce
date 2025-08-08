a9170dfdd9fb55538bd88fd92e354544dcc75714
Yiğit <yigit433.devs@gmail.com>
2025-08-08 08:29:35 +0300

    Implement routing with GoRouter and refactor MyApp to use ProviderScope

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_mini_ecommerce/features/products/products_page.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/home',
+    routes: [
+      GoRoute(
+        path: '/home',
+        builder: (context, state) => const HomeScreen(),
+      ),
+    ],
+  );
+});
